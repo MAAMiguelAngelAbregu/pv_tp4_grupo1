81bd8be66d7f4e0f7d4a7bfe4904ec172103ad43
SM-RIOS <sofirios_92@hotmail.com>
2025-05-12 01:00:15 -0300

    Creación del Formulario e implementación de las funciones creadas previamente- Rios Sofia

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
+import ProductoFormulario from "./assets/components/Formulario"
 
 function App() {
   return (
     <div>
       <h1>Hola a Todos</h1>
+      <ProductoFormulario />
     </div>
   )
 }
---
+++ src/assets/components/Formulario.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { agregarProductos } from "./productos"
+
+const ProductoFormulario= () => {
+    const [nombre, setNombre] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [precioUnitario, setPrecioUnitario] = useState('');
+    const [descuento, setDescuento] = useState(0);
+    const [stock, setStock] = useState('');
+    
+    const cargarDatos = (event) => {
+        event.preventDefault(); 
+        agregarProductos(nombre, descripcion, precioUnitario, descuento, stock);
+
+        setNombre('');
+        setDescripcion('');
+        setPrecioUnitario('');
+        setDescuento(0);
+        setStock('');    
+    };
+
+    return (
+        <div className="formu-container">
+        <form onSubmit={cargarDatos}>
+            <input
+                type="text"
+                placeholder='Nombre del producto'
+                id="nombre"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                required
+            />
+            <input
+                type="text"
+                placeholder='Descripción del producto'
+                id="descripcion"
+                value={descripcion}
+                onChange={(e) => setDescripcion(e.target.value)}
+                required
+            />            
+            <input
+                type="number"
+                placeholder='$$'
+                id="precioUnitario"
+                value={precioUnitario}
+                onChange={(e) => setPrecioUnitario(e.target.value)}
+                required
+            />
+            <input
+                type="number"
+                placeholder='Porcentaje de descuento'
+                id="descuento"
+                value={descuento}
+                onChange={(e) => setDescuento(e.target.value)}
+            />
+            <input
+                type="number"
+                placeholder='Stock disponible '
+                id="stock"
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+                required
+            />
+            <button type="submit">Agregar Producto</button>
+        </form>
+        </div>
+    );
+};
+
+export default ProductoFormulario;
