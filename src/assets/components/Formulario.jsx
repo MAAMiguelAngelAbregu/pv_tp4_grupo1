24304b9af6181d1cd91c27162b256bd7b7cb235f
Ezequiel Villalba <ezequielvillagf@gmail.com>
2025-05-12 11:48:23 -0300

    Cambios en el archivo formulario.jsx - Ezequiel Villalba

--- src/assets/components/Formulario.jsx
+++ src/assets/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { agregarProductos } from "./productos"
 
-const ProductoFormulario= () => {
+
+const ProductoFormulario = ({ onAgregar }) => {
     const [nombre, setNombre] = useState('');
     const [descripcion, setDescripcion] = useState('');
     const [precioUnitario, setPrecioUnitario] = useState('');
@@ -10,15 +10,30 @@ const ProductoFormulario= () => {
     
     const cargarDatos = (event) => {
         event.preventDefault(); 
-        agregarProductos(nombre, descripcion, precioUnitario, descuento, stock);
+        
+    const descuentoDecimal = parseFloat(descuento) / 100 || 0;
+    const precioConDescuento = parseFloat(precioUnitario) * (1 - descuentoDecimal);
+    const stockEntero = parseInt(stock);
 
-        setNombre('');
-        setDescripcion('');
-        setPrecioUnitario('');
-        setDescuento(0);
-        setStock('');    
+    const nuevoProducto = {
+      id: crypto.randomUUID(),
+      nombre,
+      descripcion,
+      precioUnitario: parseFloat(precioUnitario),
+      descuento: parseFloat(descuento),
+      precioConDescuento,
+      stock: stockEntero,
     };
 
+    onAgregar(nuevoProducto);
+     // Limpiar campos
+    setNombre('');
+    setDescripcion('');
+    setPrecioUnitario('');
+    setDescuento(0);
+    setStock('');
+  };
+    
     return (
         <div className="formu-container">
         <form onSubmit={cargarDatos}>
